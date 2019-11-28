bc42dc66c8b5152e114b9954f8ba9fcebb51026d
collion <charles.ollion@gmail.com>
2019-11-28 18:50:00 +0100

    small update on ffmpeg test

--- src/mot/tracker/video_utils.py
+++ src/mot/tracker/video_utils.py
@@ -2,16 +2,18 @@ import ffmpeg
 import os
 import cv2
 
-def split_video(input_path, output_folder, fps = 1, resolution=(1024,768)):
+def split_video(input_path, output_folder, fps = 1.5, resolution=(1024,768)):
     if not os.path.isdir(output_folder):
         os.mkdir(output_folder)
     (
         ffmpeg
         .input(input_path)
-        .output(output_folder + 'frame_%4d.jpeg', vf='fps=1', format='image2', vcodec='mjpeg')
+        .filter('fps', fps=fps, round='up')
+        .trim(start_frame=0)
+        .output(output_folder + 'frame_%4d.jpeg', format='image2', vcodec='mjpeg')
         .run()
     )
-
+# vf='fps={}'.format(fps), format='image2', vcodec='mjpeg')
 def read_folder(input_path):
     # for now, read directly from images in folder ; later from json outputs
     return [os.path.join(input_path, file) for file in  sorted(os.listdir(input_path))]
